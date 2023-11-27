b6540dc30bbaa2875982110d03cb6e245d244baf
Clair <clair.karanja@student.moringaschool.com>
2023-11-27 10:11:39 +0300

    Completed assignt.

--- lib/identifier.py
+++ lib/identifier.py
@@ -1,18 +1,1 @@
-def exactly_two_positive(a, b, c):
-    count_positive = 0
-    if a > 0:
-        count_positive += 1
-    if b > 0:
-        count_positive += 1
-    if c > 0:
-        count_positive += 1
 
-    result = count_positive == 2
-    print(f"For inputs {a}, {b}, {c}: {result}")
-    return result
-
-
-print(exactly_two_positive(2, 4, -3))  
-print(exactly_two_positive(-4, 6, 8))  
-print(exactly_two_positive(4, -6, 9))  
-print(exactly_two_positive(-4, 6, 0))  
